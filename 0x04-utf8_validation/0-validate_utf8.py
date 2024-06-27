a29910de0310377dc456653cb9e96b21a0679d0b
Dubemxe <dubemxe@gmail.com>
2024-06-27 20:14:10 +0000

    Done!/

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''
+Module for UTF-8 Validation
+'''
+
+
+def validUTF8(data):
+    """
+    Confirms if a given data set represents a valid
+    UTF-8 encoding """
+    number_bytes = 0
+
+    code_1 = 1 << 7
+    code_2 = 1 << 6
+
+    for i in data:
+
+        encode_byte = 1 << 7
+
+        if num_bytes == 0:
+
+            while encode_byte & i:
+                num_bytes += 1
+                encode_byte = encode_byte >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+
+        else:
+            if not (i & code_1 and not (i & code_2)):
+                    return False
+
+        num_bytes -= 1
+
+    if num_bytes == 0:
+        return True
+
+    return False
