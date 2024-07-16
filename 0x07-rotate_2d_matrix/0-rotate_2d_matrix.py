06a43c6ebd365d9196bd126289a63e82a247ed31
Dubemxe <dubemxe@gmail.com>
2024-07-16 11:10:53 +0000

    Pycodestyle correction

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,15 +9,14 @@ def rotate_2d_matrix(matrix):
     n = len(matrix)
 
     for i in range(n):
-        for j in range (int(n/2)):
+        for j in range(int(n/2)):
             temp = matrix[n-j-1][i]
             matrix[n-j-1][i] = matrix[j][i]
             matrix[j][i] = temp
-        
 
     """ Transpose the matrix """
     for i in range(n):
-        for j in range(i,n):
+        for j in range(i, n):
             temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
             matrix[j][i] = temp
